0c6ef0d291d232e028a641d192cca0bcb197b0fc
Sundev79 <sundev79@nutdevs.org>
2021-09-20 23:51:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const pronote = require('@dorian-eydoux/pronote-api');
 const settings = require("./settings.json");
+const { Notion } = require("./common/notion/Notion");
 
-// Exemple
+const notion = new Notion(settings.notion.integration_token);
 const url = 'https://0790036k.index-education.net/pronote/';
 const username = settings.pronote.username;
 const password = settings.pronote.password;
+const casChallenge = settings.pronote.cas;
 
-
-async function main()
-{
-    const session = await pronote.login(url, username, password, "lyceeconnecte");
+async function main() {
+    const session = await pronote.login(url, username, password, casChallenge);
     
     console.log(session.user.name); // Affiche le nom de l'élève
     console.log(session.user.studentClass.name); // Affiche la classe de l'élève
@@ -22,7 +22,10 @@ async function main()
     
     // etc. les fonctions utilisables sont 'timetable', 'marks', 'contents', 'evaluations', 'absences', 
     // 'homeworks', 'infos', et 'menu', sans oublier les champs 'user' et 'params' qui regorgent d'informations.
+
+    notion.getNotionPageBlock("b08c5a1a18de40e6ba783b8decb5cfa6").then(r => console.log(r));
 }
+
 
 main().catch(err => {
     if (err.code === pronote.errors.WRONG_CREDENTIALS.code) {
