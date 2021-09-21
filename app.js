e9abf403db92bbe5b90ca0f3af0d766e35be041a
Sundev79 <sundev79@nutdevs.org>
2021-09-21 17:14:35 +0200

    Update app.js and settings.example.json

--- app.js
+++ app.js
@@ -10,22 +10,12 @@ const username = settings.pronote.username;
 const password = settings.pronote.password;
 const casChallenge = settings.pronote.cas;
 
+
+
 async function main() {
     const session = await pronote.login(url, username, password, casChallenge);
-    
-    console.log(session.user.name); // Affiche le nom de l'élève
-    console.log(session.user.studentClass.name); // Affiche la classe de l'élève
-    
-    const timetable = await session.timetable(); // Récupérer l'emploi du temps d'aujourd'hui
-    const marks = await session.marks(); // Récupérer les notes du trimestre
-    
-    console.log(`L'élève a ${timetable.length} cours aujourd'hui`); 
-    console.log(`et a pour l'instant une moyenne de ${marks.averages.student} ce trimestre.`);
-    
-    // etc. les fonctions utilisables sont 'timetable', 'marks', 'contents', 'evaluations', 'absences', 
-    // 'homeworks', 'infos', et 'menu', sans oublier les champs 'user' et 'params' qui regorgent d'informations.
-
-    notion.getNotionPageBlock("b08c5a1a18de40e6ba783b8decb5cfa6").then(r => console.log(r));
+    let currentDate = new Date();
+    session.homeworks(new Date(currentDate.getFullYear(), 8, 1), currentDate).then(r => console.log(r));
 }
 
 
