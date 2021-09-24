8947c655a45ce9248d0baf0e6f139b957c4cadcb
Sundev79 <sundev79@nutdevs.org>
2021-09-24 09:51:13 +0200

    Update PronoteClient.js

--- common/pronote/PronoteClient.js
+++ common/pronote/PronoteClient.js
@@ -2,13 +2,13 @@ import pronote from "@dorian-eydoux/pronote-api";
 
 export default class PronoteClient {
     constructor(serverEndpoint, username, password, casChallenge) {
-
+        await openSession(serverEndpoint, username, password, casChallenge).then(e => {
+            e = curSession;
+            console.log(curSession);
+        });
     }
-
-
 }
 
 async function openSession(serverEndpoint, username, password, casChallenge) {
     return await pronote.login(serverEndpoint, username, password, casChallenge);
 }
-
