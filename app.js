fe65fa176e9fc6ef456195ae9ec02ea2286e6f81
Sundev79 <sundev79@nutdevs.org>
2021-09-27 23:16:33 +0200

    Restoring to a working state

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import Notion from "./common/notion/NotionClient.js";
 import PronoteClient from "./common/pronote/PronoteClient.js";
 import fs from "fs";
 const settings = JSON.parse(fs.readFileSync("./settings.json"));
+import pronote from '@dorian-eydoux/pronote-api';
 
 
 const notion = new Notion(settings.notion.integration_token);
@@ -9,7 +10,27 @@ const url = settings.pronote.endpoint_url;
 const username = settings.pronote.username;
 const password = settings.pronote.password;
 const casChallenge = settings.pronote.cas;
+async function main()
+{
+    const session = await pronote.login(url, username, password, casChallenge);
+    
+    console.log(session.user.name); // Affiche le nom de l'élève
+    console.log(session.user.studentClass.name); // Affiche la classe de l'élève
+    
+    const timetable = await session.timetable(); // Récupérer l'emploi du temps d'aujourd'hui
+    const marks = await session.marks(); // Récupérer les notes du trimestre
+    
+    console.log(`L'élève a ${timetable.length} cours aujourd'hui`); 
+    console.log(`et a pour l'instant une moyenne de ${marks.averages.student} ce trimestre.`);
+    
+    // etc. les fonctions utilisables sont 'timetable', 'marks', 'contents', 'evaluations', 'absences', 
+    // 'homeworks', 'infos', et 'menu', sans oublier les champs 'user' et 'params' qui regorgent d'informations.
+}
 
-
-let pronote = new PronoteClient(url, username, password, casChallenge);
-console.log(pronote.serverEndPoint);
+main().catch(err => {
+    if (err.code === pronote.errors.WRONG_CREDENTIALS.code) {
+        console.error('Mauvais identifiants');    
+    } else {
+        console.error(err);
+    }
+});
--- common/pronote/PronoteClient.js
+++ common/pronote/PronoteClient.js
@@ -6,8 +6,8 @@ export default class PronoteClient {
     curSession;
     serverEndPoint;
     
-    constructor(_endpoint, username, password, casChallenge) {
-        _endpoint = this.serverEndPoint;
+    constructor(_eendpoint, username, password, casChallenge) {
+        _eendpoint = this.serverEndPoint;
         console.log(casChallenge);
         this.curSession = openSession(this.serverEndPoint, username, password, casChallenge);
         return this.curSession;
